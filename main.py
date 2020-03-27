b11d32a53eb2e50c453a362dcec87eadc075b342
Sam Harris <sam@kaggle.com>
2020-03-27 12:47:42 -0700

    typo

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ parser.add_argument(
     "--host", type=str, help="http-server Host (default=127.0.0.1)."
 )
 parser.add_argument(
-    "--subprocess-agnets", type=bool, help="Use an inner subprocess to isolate each agent."
+    "--subprocess-agents", type=bool, help="Use an inner subprocess to isolate each agent."
 )
 
 
