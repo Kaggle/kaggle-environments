17e5669722d0953503fe3f3db5b46fcf8e113e65
Sam Harris <sam@kaggle.com>
2020-05-13 13:39:37 -0700

    0.3.0 New halite rules, bug fixes, and visualizer updates

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -19,7 +19,7 @@ from .core import *
 from . import errors
 from . import utils
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 __all__ = ["Agent", "environments", "errors", "evaluate",
            "make", "register", "utils", "version"]
