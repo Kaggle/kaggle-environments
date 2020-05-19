18049fcdb6d4e2c262597a26ff8dc4ea868a7330
Sam Harris <sam@kaggle.com>
2020-05-19 15:58:26 -0700

    Added missing 0.3.3 version bump

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -19,7 +19,7 @@ from .core import *
 from . import errors
 from . import utils
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 __all__ = ["Agent", "environments", "errors", "evaluate",
            "make", "register", "utils", "version"]
