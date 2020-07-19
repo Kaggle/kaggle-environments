Fix max int 32 calculation, disable parallelization locally, add isProduction parameter to configuration, switch to perf_counter for timing

kaggle_environments/envs/halite/halite.py
@@ -89,7 +89,8 @@ def populate_board(state, env):
 
     # Set seed for random number generators
     if not hasattr(config, "randomSeed"):
-        config.randomSeed = randrange(sys.maxsize)
+        max_int_32 = 2 ** 31 - 1
+        config.randomSeed = randrange(max_int_32)
 
     np.random.seed(config.randomSeed)
     seed(config.randomSeed)