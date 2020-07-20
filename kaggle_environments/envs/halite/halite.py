e93176a6042e120a776cb12963cde6fdebd55abd
Sam Harris <harrisse2@gmail.com>
2020-07-19 23:56:30 -0700

    Use bit shift instead of pow of 2 to calculate max 32 bit int

--- kaggle_environments/envs/halite/halite.py
+++ kaggle_environments/envs/halite/halite.py
@@ -89,7 +89,7 @@ def populate_board(state, env):
 
     # Set seed for random number generators
     if not hasattr(config, "randomSeed"):
-        max_int_32 = 2 ** 31 - 1
+        max_int_32 = (1 << 31) - 1
         config.randomSeed = randrange(max_int_32)
 
     np.random.seed(config.randomSeed)
