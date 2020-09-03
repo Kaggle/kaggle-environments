c82c0bc90d07ef269680d872a48a64f07a0d7db3
qstanczyk <51751254+qstanczyk@users.noreply.github.com>
2020-09-03 20:52:23 +0200

    Update handling of TeamNames (#81)

--- kaggle_environments/envs/football/football.py
+++ kaggle_environments/envs/football/football.py
@@ -130,12 +130,12 @@ def interpreter(state, env):
                 assert not env.configuration.render, "Render is not supported inside notebook environment."
 
             env.football_video_path = None
-            if 'agents' in env.info:
-                names = env.info['agents']
+            if 'TeamNames' in env.info:
+                names = env.info['TeamNames']
                 assert len(names) == 2
                 other_config_options['custom_display_stats'] = [
-                    'LEFT PLAYER: %s' % names[0]['teamName'],
-                    'RIGHT PLAYER: %s' % names[1]['teamName']]
+                    'LEFT PLAYER: %s' % names[0],
+                    'RIGHT PLAYER: %s' % names[1]]
             m_envs[env.configuration.id] = football_env().create_environment(
                 env_name=env.configuration.scenario_name,
                 stacked=False,
