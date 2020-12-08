77dc6e32dc0d167a6ffbfdfb00b07ec06b3ba442
pculliton <philculliton@google.com>
2020-12-07 19:30:42 -0500

    Update __init__.py

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -21,7 +21,7 @@ from .main import http_request
 from . import errors
 from . import utils
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 __all__ = ["Agent", "environments", "errors", "evaluate", "http_request",
            "make", "register", "utils", "__version__",
