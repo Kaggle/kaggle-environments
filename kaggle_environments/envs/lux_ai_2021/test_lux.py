db61ffb6be57162c18b54895a7a4ed9acbc9f096
Stone Tao <stonezt2019@gmail.com>
2021-04-15 08:46:20 -0700

    use js bots in tests and published package

--- kaggle_environments/envs/lux_ai_2021/test_lux.py
+++ kaggle_environments/envs/lux_ai_2021/test_lux.py
@@ -1,9 +1,16 @@
 from kaggle_environments import make
-from .agents import random_agent
+from .agents import random_agent, js_agent_random, js_agent_slow_expand
 
 def test_lux_completes():
     env = make("lux_ai_2021", configuration={})
     env.run([random_agent, random_agent])
+    json = env.toJSON()
+    assert json["name"] == "lux_ai_2021"
+    assert json["statuses"] == ["DONE", "DONE"]
+
+def test_js_agents():
+    env = make("lux_ai_2021", configuration={})
+    env.run([js_agent_slow_expand, js_agent_random])
     json = env.toJSON()
     assert json["name"] == "lux_ai_2021"
     assert json["statuses"] == ["DONE", "DONE"]
