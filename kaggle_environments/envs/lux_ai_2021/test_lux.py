24024bd488ae226f6d5c878c57fc67c1ef18c235
Stone Tao <stonezt2019@gmail.com>
2021-07-08 15:06:26 -0400

    Update test_lux.py

--- kaggle_environments/envs/lux_ai_2021/test_lux.py
+++ kaggle_environments/envs/lux_ai_2021/test_lux.py
@@ -1,17 +1,17 @@
 import sys
 from kaggle_environments import make
-from .agents import random_agent, js_simple_agent, organic_agent
+from .agents import random_agent, js_simple_agent, simple_agent
 
 def test_lux_completes():
     env = make("lux_ai_2021", configuration={})
-    env.run([random_agent, organic_agent])
+    env.run([random_agent, simple_agent])
     json = env.toJSON()
     assert json["name"] == "lux_ai_2021"
     assert json["statuses"] == ["DONE", "DONE"]
 
 def test_js_agents():
     env = make("lux_ai_2021", configuration={})
-    env.run([organic_agent, js_simple_agent])
+    env.run([simple_agent, js_simple_agent])
     json = env.toJSON()
     assert json["name"] == "lux_ai_2021"
     assert json["statuses"] == ["DONE", "DONE"]
