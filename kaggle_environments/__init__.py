d1b52004fbc23af568f4867e3ef7b64144df165e
Bovard Doerschuk-Tiberi <bovard@google.com>
2021-08-06 01:53:44 +0000

    Update to 1.8.5

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -21,7 +21,7 @@ from .main import http_request
 from . import errors
 from . import utils
 
-__version__ = "1.8.4"
+__version__ = "1.8.5"
 
 __all__ = ["Agent", "environments", "errors", "evaluate", "http_request",
            "make", "register", "utils", "__version__",
