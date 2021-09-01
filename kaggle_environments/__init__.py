a5d9f75dd231c9971bdfc92aacda1420f7eed112
Bovard Doerschuk-Tiberi <bovard@google.com>
2021-09-01 05:13:32 +0000

    Update to 1.8.12, bump lux to 3.1.0

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -21,7 +21,7 @@ from .main import http_request
 from . import errors
 from . import utils
 
-__version__ = "1.8.11"
+__version__ = "1.8.12"
 
 __all__ = ["Agent", "environments", "errors", "evaluate", "http_request",
            "make", "register", "utils", "__version__",
