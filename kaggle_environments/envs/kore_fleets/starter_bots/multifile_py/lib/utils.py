5cab0046f80134c1a655c693d41a02eb03a6beb9
Bovard Doerschuk-Tiberi <bovard@google.com>
2022-04-01 05:35:19 +0000

    remove unused

--- kaggle_environments/envs/kore_fleets/starter_bots/multifile_py/lib/utils.py
+++ kaggle_environments/envs/kore_fleets/starter_bots/multifile_py/lib/utils.py
@@ -1,27 +1,4 @@
-import math
 from kaggle_environments.envs.kore_fleets.helpers import *
-from random import choice, randint, randrange, sample, seed, random
-
-def check_path(board, start, dirs, dist_a, dist_b, collection_rate):
-    kore = 0
-    npv = .98
-    current = start
-    steps = 2 * (dist_a + dist_b + 2)
-    for idx, d in enumerate(dirs):
-        for _ in range((dist_a if idx % 2 == 0 else dist_b) + 1):
-            current = current.translate(d.to_point(), board.configuration.size)
-            kore += int((board.cells.get(current).kore or 0) * collection_rate)
-    return math.pow(npv, steps) * kore / (2 * (dist_a + dist_b + 2))
-
-def check_location(board, loc, me):
-    if board.cells.get(loc).shipyard and board.cells.get(loc).shipyard.player.id == me.id:
-        return 0
-    kore = 0
-    for i in range(-3, 4):
-        for j in range(-3, 4):
-            pos = loc.translate(Point(i, j), board.configuration.size)
-            kore += board.cells.get(pos).kore or 0
-    return kore
 
 def get_closest_enemy_shipyard(board, position, me):
     min_dist = 1000000
@@ -35,25 +12,3 @@ def get_closest_enemy_shipyard(board, position, me):
             enemy_shipyard = shipyard
     return enemy_shipyard
     
-def get_shortest_flight_path_between(position_a, position_b, size, trailing_digits=False):
-    mag_x = 1 if position_b.x > position_a.x else -1
-    abs_x = abs(position_b.x - position_a.x)
-    dir_x = mag_x if abs_x < size/2 else -mag_x
-    mag_y = 1 if position_b.y > position_a.y else -1
-    abs_y = abs(position_b.y - position_a.y)
-    dir_y = mag_y if abs_y < size/2 else -mag_y
-    flight_path_x = ""
-    if abs_x > 0:
-        flight_path_x += "E" if dir_x == 1 else "W"
-        flight_path_x += str(abs_x - 1) if (abs_x - 1) > 0 else ""
-    flight_path_y = ""
-    if abs_y > 0:
-        flight_path_y += "N" if dir_y == 1 else "S"
-        flight_path_y += str(abs_y - 1) if (abs_y - 1) > 0 else ""
-    if not len(flight_path_x) == len(flight_path_y):
-        if len(flight_path_x) < len(flight_path_y):
-            return flight_path_x + (flight_path_y if trailing_digits else flight_path_y[0])
-        else:
-            return flight_path_y + (flight_path_x if trailing_digits else flight_path_x[0])
-    return flight_path_y + (flight_path_x if trailing_digits or not flight_path_x else flight_path_x[0]) if random() < .5 else flight_path_x + (flight_path_y if trailing_digits or not flight_path_y else flight_path_y[0])
-
