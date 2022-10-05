ab88a063e091e969c4bc26abe8595c78b4a59e30
stone_tao <stonezt2019@gmail.com>
2022-10-04 17:24:45 -0700

    bug fixes with weather, remove TODOs

--- kaggle_environments/envs/lux_ai_2022/luxai2022/config.py
+++ kaggle_environments/envs/lux_ai_2022/luxai2022/config.py
@@ -67,15 +67,14 @@ class EnvConfig:
 
     #### Factores ####
     FACTORY_PROCESSING_RATE_WATER: int = 50
-    ICE_WATER_RATIO: int = 2
+    ICE_WATER_RATIO: int = 1
     FACTORY_PROCESSING_RATE_METAL: int = 50
-    ORE_METAL_RATIO: int = 2
+    ORE_METAL_RATIO: int = 1
     FACTORY_CHARGE: int = 50
     FACTORY_WATER_CONSUMPTION: int = 1
 
 
     #### Units ####
-    # TODO FILL IN POWER COSTS FOR ACTIONS
     ROBOTS: Dict[str, UnitConfig] = dataclasses.field(
         default_factory=lambda: dict(
         LIGHT=UnitConfig(
