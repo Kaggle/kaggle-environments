f6f45f1e08bf6fc7ee8ea55527a55c9b7eaf7304
Bovard Doerschuk-Tiberi <bovard@google.com>
2022-10-31 23:53:52 +0000

    1.10.2

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -21,7 +21,7 @@ from .main import http_request
 from . import errors
 from . import utils
 
-__version__ = "1.10.1"
+__version__ = "1.10.2"
 
 __all__ = ["Agent", "environments", "errors", "evaluate", "http_request",
            "make", "register", "utils", "__version__",
