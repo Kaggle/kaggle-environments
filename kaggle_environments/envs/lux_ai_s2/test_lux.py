5f25fd7c0b40011788f0aaefcdec4f4514dd79a5
stone_tao <stonezt2019@gmail.com>
2023-01-24 17:29:26 -0800

    fix typedDict and board seeding bug

--- kaggle_environments/envs/lux_ai_s2/test_lux.py
+++ kaggle_environments/envs/lux_ai_s2/test_lux.py
@@ -3,7 +3,7 @@ from kaggle_environments import make
 from .agents import random_agent
 
 def test_lux_completes():
-    env = make("lux_ai_s2", configuration={})
+    env = make("lux_ai_s2", debug=True)
     env.run([random_agent, random_agent])
     json = env.toJSON()
     assert json["name"] == "lux_ai_s2"
