7f08eaad421bd8d3b7152b119fd8c5d000aaee43
Bovard Doerschuk-Tiberi <bovard@google.com>
2024-05-29 06:55:09 +0000

    made custom agents

--- kaggle_environments/envs/llm_20_questions/test_llm_20_questions.py
+++ kaggle_environments/envs/llm_20_questions/test_llm_20_questions.py
@@ -1,10 +1,17 @@
 
 from kaggle_environments import make
-from llm_20_questions import random_guesser, random_answerer
+
+def custom_questioner(obs):
+    if obs.turnType == "guess":
+        return "banana"
+    return "Is it a banana?"
+
+def custom_answerer():
+    return "no"
 
 def test_lux_completes():
     env = make("llm_20_questions", debug=True)
-    env.run([random_guesser, random_answerer, random_guesser, random_answerer])
+    env.run([custom_questioner, custom_answerer, custom_questioner, custom_answerer])
     json = env.toJSON()
     assert json["name"] == "llm_20_questions"
     assert json["statuses"] == ["DONE", "DONE", "DONE", "DONE"]
