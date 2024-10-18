ed3c1bb3d1c77ad90abdc8fbe6e814b7bae8360c
stone_tao <stonezt2019@gmail.com>
2024-10-18 13:31:03 -0700

    make random agent kaggle compatible with imports

--- kaggle_environments/envs/lux_ai_s3/test_lux.py
+++ kaggle_environments/envs/lux_ai_s3/test_lux.py
@@ -3,8 +3,8 @@ from kaggle_environments import make
 from .agents import random_agent
 
 def test_lux_completes():
-    env = make("lux_ai_s2", debug=True)
+    env = make("lux_ai_s3", debug=True)
     env.run([random_agent, random_agent])
     json = env.toJSON()
-    assert json["name"] == "lux_ai_s2"
+    assert json["name"] == "lux_ai_s3"
     assert json["statuses"] == ["DONE", "DONE"]
