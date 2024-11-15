62c6f4ce3f7c44f8083f403ed7589d032691fa1d
Bovard Doerschuk-Tiberi <dt.bovard@gmail.com>
2024-11-15 08:29:05 -1000

    fix seed bug

--- kaggle_environments/envs/chess/chess.py
+++ kaggle_environments/envs/chess/chess.py
@@ -172,7 +172,7 @@ def interpreter(state, env):
         seen_positions = defaultdict(int)
         game_start_position = math.floor(random.random() * len(OPENINGS))
         if "seed" in env.configuration:
-            seed = int(env.configuration["seed"]) % OPENINGS[game_start_position]
+            seed = int(env.configuration["seed"]) % len(OPENINGS)
             game_start_position = seed & len(OPENINGS)
         state[0].observation.board = OPENINGS[game_start_position]
         state[1].observation.board = OPENINGS[game_start_position]
