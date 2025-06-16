71cd594c08fbafced100bc7df5718d8b6e97c300
jhtschultz <jhtschultz@google.com>
2025-06-16 22:13:19 +0000

    Fix indentation in test_open_spiel.py.

--- kaggle_environments/envs/open_spiel/test_open_spiel.py
+++ kaggle_environments/envs/open_spiel/test_open_spiel.py
@@ -5,21 +5,28 @@ import pyspiel
 from . import open_spiel as open_spiel_env
 
 
+# Expected that not all pyspiel registered games can be registered as Kaggle
+# envs (e.g. does not yet support simultaneous move games), but should register
+# at least this many
+_REGISTERED_GAMES_THRESHOLD = 50
+
+
 class OpenSpielEnvTest(absltest.TestCase):
+
   def test_envs_load(self):
     envs = open_spiel_env._register_game_envs(
         [game_type.short_name for game_type in pyspiel.registered_games()]
     )
-    self.assertTrue(len(envs) > 50)
+    self.assertTrue(len(envs) > _REGISTERED_GAMES_THRESHOLD)
 
-def test_tic_tac_toe_playthrough(self):
-  envs = open_spiel_env._register_game_envs(["tic_tac_toe"])
-  print(envs)
-  env = make("open_spiel_tic_tac_toe", debug=True)
-  env.run(["random", "random"])
-  json = env.toJSON()
-  self.assertEqual(json["name"], "open_spiel_tic_tac_toe")
-  self.assertTrue(all([status == "DONE" for status in json["statuses"]]))
+  def test_tic_tac_toe_playthrough(self):
+    envs = open_spiel_env._register_game_envs(["tic_tac_toe"])
+    print(envs)
+    env = make("open_spiel_tic_tac_toe", debug=True)
+    env.run(["random", "random"])
+    json = env.toJSON()
+    self.assertEqual(json["name"], "open_spiel_tic_tac_toe")
+    self.assertTrue(all([status == "DONE" for status in json["statuses"]]))
 
 
 if __name__ == '__main__':
