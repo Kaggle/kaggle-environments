166a7affc794520b1bad1bcf2fbcc0c8d84f6cd8
hannw <lostkingwwwth@gmail.com>
2025-07-10 17:27:14 -0700

    refactored werewolf players to receive observations in message queue. game interpreter working.

--- kaggle_environments/envs/werewolf/game/states.py
+++ kaggle_environments/envs/werewolf/game/states.py
@@ -1,103 +1,12 @@
+from typing import List, Dict, Optional, Any, Union
+from collections import defaultdict
+from functools import cached_property
 
-from abc import ABC
-from enum import Enum
-from typing import List, Dict, Optional, Any, Set, Union
+from pydantic import BaseModel, PrivateAttr, Field, computed_field
 
-
-from pydantic import BaseModel, PrivateAttr, Field
-
-# Renamed to avoid clash
-from .roles import Player, Phase, RoleConst, Team
-
-
-class HistoryEntryType(str, Enum):
-    GAME_START = "game_start"
-    PHASE_CHANGE = "phase_change"
-    ACTION_RESULT = "action_result"
-    ELIMINATION = "elimination"
-    VOTE_ACTION = "vote_action"
-    VOTE_RESULT = "vote_result"
-    DISCUSSION = "discussion"
-    BIDDING_INFO = "bidding_info"
-    GAME_END = "game_end"
-    MODERATOR_ANNOUNCEMENT = "moderator_announcement"
-    ERROR = "error"
-
-
-class DataEntry(BaseModel, ABC):
-    pass
-
-
-class HistoryEntry(BaseModel):
-    day: int  # Day number, 0 for initial night
-    phase: Phase
-    entry_type: HistoryEntryType
-    description: str
-    public: bool = False
-    visible_to: Set[str] = Field(default_factory=set)
-    data: Optional[DataEntry]
-
-
-class GameStartDataEntry(DataEntry):
-    player_ids: List[str]
-    number_of_players: int
-    role_counts: Dict[str, int]
-    team_member_counts: Dict[str, int]
-    day_discussion_protocol_name: str
-    day_discussion_protocol_rule: str
-    night_werewolf_discussion_protocol_name: str
-    night_werewolf_discussion_protocol_rule: str
-    day_voting_protocol_name: str
-    day_voting_protocol_rule: str
-
-
-class GameStartRoleDataEntry(DataEntry):
-    player_id: str
-    team: str
-    role: str
-    rule_of_role: str
-
-
-class AskDoctorSaveDataEntry(DataEntry):
-    valid_candidates: List[str]
-    action_json_schema: str
-
-
-class AskSeerRevealDataEntry(DataEntry):
-    valid_candidates: List[str]
-    action_json_schema: str
-
-
-class AskWerewolfVotingDataEntry(DataEntry):
-    valid_targets: List[str]
-    alive_werewolve_player_ids: List[str]
-    voting_protocol_name: str
-    voting_protocol_rule: str
-    action_json_schema: str
-
-
-class SeerInspectResultDataEntry(DataEntry):
-    actor_id: str
-    target_id: str
-    role: str
-    team: str
-
-
-class WerewolfNightVoteDataEntry(DataEntry):
-    actor_id: str
-    target_id: str
-    reasoning: Optional[str]
-
-
-class WerewolfNightEliminationElectedDataEntry(DataEntry):
-    """This record the elected elimination target by werewolves."""
-    elected_target_player_id: str
-
-
-class WerewolfNightEliminationDataEntry(DataEntry):
-    """This record the one eventually got eliminated by werewolves without doctor safe."""
-    eliminated_player_id: str
-    eliminated_player_role_name: str
+from .records import HistoryEntryType, DataEntry, HistoryEntry
+from .roles import Player
+from .consts import Phase, Team, RoleConst
 
 
 class GameState(BaseModel):
@@ -107,6 +16,13 @@ class GameState(BaseModel):
     history: Dict[int, List[HistoryEntry]] = Field(default_factory=dict)
     wallet: dict[str, int] = Field(default_factory=dict)
     _id_to_player: Dict[str, Player] = PrivateAttr(default_factory=dict)
+    _history_entry_by_type: Dict[HistoryEntryType, List[HistoryEntry]] = PrivateAttr(
+        default_factory=lambda: defaultdict(list))
+
+    @computed_field
+    @cached_property
+    def all_player_ids(self) -> List[str]:
+        return [player.id for player in self.players]
 
     def model_post_init(self, context: Any, /) -> None:
         self._id_to_player = {p.id: p for p in self.players}
@@ -114,19 +30,22 @@ class GameState(BaseModel):
     def get_player_by_id(self, pid: str):
         return self._id_to_player.get(pid)
 
+    def get_players_by_team(self, team: Team):
+        return [p for p in self.players if p.role.team == team]
+
     def alive_players(self):
         return [p for p in self.players if p.alive]
 
     def alive_players_by_role(self, role: RoleConst):
         return [p for p in self.alive_players() if p.role.name == role]
-    
+
     def alive_players_by_team(self, team: Team):
         return [p for p in self.alive_players() if p.role.team == team]
-    
+
     def alive_player_counts_per_role(self):
         counts = {role: len(self.alive_players_by_role(role)) for role in RoleConst}
         return counts
-    
+
     def alive_player_counts_per_team(self):
         return {team: len(self.alive_players_by_team(team)) for team in Team}
 
@@ -143,8 +62,12 @@ class GameState(BaseModel):
     def queue_doctor_save(self, target: Player):
         self._night_doctor_save_queue.append(target.id)
 
+    def get_history_by_type(self, entry_type: HistoryEntryType) -> List[HistoryEntry]:
+        return self._history_entry_by_type[entry_type]
+
     def add_history_entry(self, description: str, entry_type: HistoryEntryType, public: bool,
-                          visible_to: Optional[List[str]] = None, data: Optional[Union[DataEntry,Dict[str, Any]]] = None):
+                          visible_to: Optional[List[str]] = None, data: Optional[Union[
+                DataEntry, Dict[str, Any]]] = None):
         # Night 0 will use day_count 0, Day 1 will use day_count 1, etc.
         day_key = self.day_count
         self.history.setdefault(day_key, [])
@@ -153,6 +76,17 @@ class GameState(BaseModel):
                              visible_to=set(visible_to) if visible_to is not None else set(),
                              data=data)
         self.history[day_key].append(entry)
+        self._history_entry_by_type[entry_type].append(entry)
+
+        # observers message pushing below
+        if public:
+            for player in self.players:
+                player.update(entry)
+        else:
+            for player_id in visible_to:
+                player = self.get_player_by_id(player_id)
+                if player:
+                    player.update(entry)
 
     def eliminate_player(self, pid: str):
         player = self.get_player_by_id(pid)
