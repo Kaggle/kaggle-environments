710935481cbea076f7af558410e95f9fb1db0fc3
jhtschultz <jhtschultz@google.com>
2025-07-18 03:01:46 +0000

    Terminate episode gracefully when an invalid action is submitted.

--- kaggle_environments/envs/open_spiel/test_open_spiel.py
+++ kaggle_environments/envs/open_spiel/test_open_spiel.py
@@ -19,14 +19,48 @@ class OpenSpielEnvTest(absltest.TestCase):
     )
     self.assertTrue(len(envs) > _REGISTERED_GAMES_THRESHOLD)
 
-  def test_tic_tac_toe_playthrough(self):
+  def test_tic_tac_toe_agent_playthrough(self):
     envs = open_spiel_env._register_game_envs(["tic_tac_toe"])
-    print(envs)
     env = make("open_spiel_tic_tac_toe", debug=True)
     env.run(["random", "random"])
     json = env.toJSON()
     self.assertEqual(json["name"], "open_spiel_tic_tac_toe")
     self.assertTrue(all([status == "DONE" for status in json["statuses"]]))
+
+  def test_tic_tac_toe_manual_playthrough(self):
+    envs = open_spiel_env._register_game_envs(["tic_tac_toe"])
+    env = make("open_spiel_tic_tac_toe", debug=True)
+    env.reset()
+    env.step([{"submission": -1}, {"submission": -1}])  # Initial setup step.
+    env.step([{"submission": 0}, {"submission": -1}])
+    env.step([{"submission": -1}, {"submission": 1}])
+    env.step([{"submission": 3}, {"submission": -1}])
+    env.step([{"submission": -1}, {"submission": 4}])
+    env.step([{"submission": 6}, {"submission": -1}])
+    self.assertTrue(env.done)
+    self.assertEqual(env.toJSON()["rewards"], [1, -1])
+
+  def test_invalid_action(self):
+    envs = open_spiel_env._register_game_envs(["tic_tac_toe"])
+    env = make("open_spiel_tic_tac_toe", debug=True)
+    env.reset()
+    for i in range(5):  # Try repeatedly applying an illegal action
+      env.step([
+          {"submission": pyspiel.INVALID_ACTION},
+          {"submission": pyspiel.INVALID_ACTION},
+      ])
+      if env.done:
+        break
+    self.assertEqual(i, 1)  # Zeroth step is setup step, should fail next step.
+    json = env.toJSON()
+    self.assertTrue(all([status == "DONE" for status in json["statuses"]]))
+    self.assertEqual(
+        json["rewards"],
+        [
+            open_spiel_env.DEFAULT_INVALID_ACTION_REWARD,
+            -open_spiel_env.DEFAULT_INVALID_ACTION_REWARD,
+        ]
+    )
 
 
 if __name__ == '__main__':
