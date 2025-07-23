eb97a8c26d8713a9e7931cdfee188104726682de
Bovard Doerschuk-Tiberi <bovard@google.com>
2025-07-23 10:59:34 -1000

    Extend timeouts on open spiel games

--- kaggle_environments/envs/open_spiel/open_spiel.py
+++ kaggle_environments/envs/open_spiel/open_spiel.py
@@ -44,8 +44,8 @@ for proxy_file in GAMES_DIR.glob("**/*_proxy.py"):
 # --- Constants ---
 DEFAULT_INVALID_ACTION_REWARD = -1
 
-DEFAULT_ACT_TIMEOUT = 300
-DEFAULT_RUN_TIMEOUT = 7200
+DEFAULT_ACT_TIMEOUT = 600 # ten minutes
+DEFAULT_RUN_TIMEOUT = 14400 # six hours
 # Buffer in addition to max game length to account for timeouts, retrys, etc.
 DEFAULT_STEP_BUFFER = 100
 # TODO(jhtschultz): Add individual game descriptions.
