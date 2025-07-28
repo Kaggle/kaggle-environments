bf1d1c8ace360ff25eccc1fcaec244ebe0b77056
kiyota hidekazu <kiyota@heroz.co.jp>
2025-07-28 17:48:57 +0900

    New environment cabt

---
+++ kaggle_environments/envs/cabt/ptcg/sim.py
@@ -0,0 +1,37 @@
+import ctypes
+import os
+
+class SerialData(ctypes.Structure):
+    _fields_ = [
+        ("json", ctypes.c_char_p),
+        ("data", ctypes.POINTER(ctypes.c_ubyte)),
+        ("count", ctypes.c_int),
+        ("selectPlayer", ctypes.c_int)
+    ]
+
+if os.name == 'nt':
+    lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ptcg.dll")
+else:
+    lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libptcg.so")
+lib = ctypes.cdll.LoadLibrary(lib_path)
+
+lib.GameInitialize()
+
+lib.BattleStart.restype = ctypes.c_void_p
+lib.BattleStart.argtypes = [ctypes.POINTER(ctypes.c_int)]
+
+lib.BattleFinish.argtypes = [ctypes.c_void_p]
+
+lib.GetBattleData.restype = SerialData
+lib.GetBattleData.argtypes = [ctypes.c_void_p]
+
+lib.Select.restype = ctypes.c_int
+lib.Select.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+
+lib.VisualizeData.restype = ctypes.c_char_p
+lib.VisualizeData.argtypes = [ctypes.c_void_p]
+
+class Battle:
+    battle_ptr = None
+    obs = None
+    raminingTime = [[], []]
