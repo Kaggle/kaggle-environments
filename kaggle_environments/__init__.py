0ce8167656bb2a2789029badc646ed77d1aad05c
Bovard Doerschuk-Tiberi <bovard@google.com>
2025-08-07 00:52:42 +0000

    1.17.11 viewer fixes

--- kaggle_environments/__init__.py
+++ kaggle_environments/__init__.py
@@ -20,7 +20,7 @@ from .core import *
 from .main import http_request
 from . import errors
 
-__version__ = "1.17.10"
+__version__ = "1.17.11"
 
 __all__ = ["Agent", "environments", "errors", "evaluate", "http_request",
            "make", "register", "utils", "__version__",
