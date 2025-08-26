eef04b2a1f11041e1fdbf66f1f61d9d5e019686b
SohierDane <sohier@google.com>
2025-08-26 17:54:34 +0000

    Correct comments

--- docker/bash.sh
+++ docker/bash.sh
@@ -1,2 +1,3 @@
 # Start the orchestrator container in Docker
+# The double slash in the test directory is to force it to translate to a Linux path rather than a Windows path
 docker run -it --entrypoint //bin/bash --rm --name python-simulations python-simulations-cpu
--- docker/bashsc.sh
+++ docker/bashsc.sh
@@ -1,2 +1,3 @@
 # Start the orchestrator container in gVisor
+# The double slash in the test directory is to force it to translate to a Linux path rather than a Windows path
 docker run --runtime=runsc -it --cpus="0.8" --memory="4g" --entrypoint //bin/bash --rm --name python-simulations python-simulations-cpu
