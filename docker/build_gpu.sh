a32426d8713dadebe57a8f44f2e4e32638b0a2a8
Michael Aaron <michaelaaron@google.com>
2025-10-21 15:03:29 -0600

    Optimize Cloudbuild
    - Use a private pool with beefier machines and co-location with AR in us-east-1
    - Use buildkit to optimize layer caching
    - Parallellize the CPU/GPU builds
    - Clean up some UV caching for slightly smaller layers for a faster push
    - Make the node/npm build a bit more sane
    - Clean up unused open_spiel.Dockerfile

--- docker/build_gpu.sh
+++ docker/build_gpu.sh
@@ -2,5 +2,5 @@
 path=$(dirname $0)
 # cd to the parent directory to include kaggle_environments folder in Docker build context
 cd $path/..
-docker build -f ./docker/gpu.Dockerfile -t python-simulations-gpu .
+docker build -f ./docker/Dockerfile --target gpu --build-arg BASE_IMAGE=gcr.io/kaggle-gpu-images/python:latest -t python-simulations-gpu .
 cd -
