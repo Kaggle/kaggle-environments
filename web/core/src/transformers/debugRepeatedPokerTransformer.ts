a931eb0cf9f91c39fab22c1ff556f3a69a15fa0d
Michael Aaron <michaelaaron@google.com>
2025-10-30 13:28:28 -0600

    Now with scrolling

--- web/core/src/transformers/debugRepeatedPokerTransformer.ts
+++ web/core/src/transformers/debugRepeatedPokerTransformer.ts
@@ -3,8 +3,12 @@ import { PokerReplay } from './poker-replay-types';
 export const debugRepeatedPokerTransformer = (environment: any) => {
   const repeatedPokerReplay: PokerReplay = environment as PokerReplay;
   const repeatedPokerReplayStateHistory = environment.info.stateHistory;
-  repeatedPokerReplayStateHistory.forEach((stateHistory: string) => {
-    console.log(JSON.parse(stateHistory));
+  const parsedStateHistorySteps = repeatedPokerReplayStateHistory.map((stateHistory: string) => {
+    const stateHistoryObject = JSON.parse(stateHistory);
+    stateHistoryObject['current_universal_poker_json'] = JSON.parse(stateHistoryObject['current_universal_poker_json']);
+    return stateHistoryObject;
   });
-  return repeatedPokerReplay;
+  return repeatedPokerReplay.steps;
+
+  //return parsedStateHistorySteps;
 };
