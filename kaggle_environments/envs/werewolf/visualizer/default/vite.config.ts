184558d5a4b0dbaa8513ac169976c6c28ca02ef8
Michael Aaron <michaelaaron@google.com>
2025-11-10 17:48:01 -0700

    [Werewolf] Move new werewolf visualizer to use static framework

---
+++ kaggle_environments/envs/werewolf/visualizer/default/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig, mergeConfig } from 'vite';
+import baseConfig from '../../../../../web/vite.config.base';
+
+// https://vitejs.dev/config/
+export default mergeConfig(baseConfig, defineConfig({
+  publicDir: 'replays',
+}));
