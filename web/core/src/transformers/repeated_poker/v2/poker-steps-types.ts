613e3f8d61fb9287c209b2764fce893754613ecb
Michael Aaron <michaelaaron@google.com>
2025-11-11 09:10:53 -0700

    [Repeated Poker] Clear chips between streets at the transformer level

--- web/core/src/transformers/repeated_poker/v2/poker-steps-types.ts
+++ web/core/src/transformers/repeated_poker/v2/poker-steps-types.ts
@@ -26,6 +26,7 @@ export interface RepeatedPokerStepPlayer extends BaseGamePlayer {
   cards: string;
   chipStack: number;
   currentBet: number;
+  currentBetForStreet: number;
   reward: number | null;
   isDealer: boolean;
   isWinner: boolean;
