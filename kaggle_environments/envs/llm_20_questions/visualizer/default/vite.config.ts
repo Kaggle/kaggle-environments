01eb29af9135f29f57d43fd7ea4e929aa0d47795
Michael Aaron <michaelaaron@google.com>
2025-11-21 08:53:49 -0700

    [Visualizer Refactor] migrate LLM 20 Questions to vite framework

---
+++ kaggle_environments/envs/llm_20_questions/visualizer/default/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, mergeConfig } from 'vite';
+import baseConfig from '../../../../../web/vite.config.base';
+
+// https://vitejs.dev/config/
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    publicDir: 'replays',
+  })
+);
