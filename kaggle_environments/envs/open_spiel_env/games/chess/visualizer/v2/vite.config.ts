4052a29c3b153c87f951810398ad1f9a4c1c063f
Michael Aaron <develra@users.noreply.github.com>
2026-01-28 16:52:46 -0700

    Add new visualizer folder for open_spiel chess (#665)
    Literally a copy/paste of the default - will give this to Stink to run
    with, I'll put up a P.R. in kaggleazure in a moment to be able to select
    a different visualizer.

---
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig, mergeConfig } from 'vite';
+import baseConfig from '../../../../../../../web/vite.config.base';
+
+// https://vitejs.dev/config/
+export default mergeConfig(baseConfig, defineConfig({
+}));
