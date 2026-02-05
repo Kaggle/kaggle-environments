06d6a906cdf7c489403122bd63b80653191554d9
Domino Weir <dominoweir@google.com>
2026-02-05 17:34:07 +0000

    Add playwright rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import jsxA11Y from "eslint-plugin-jsx-a11y";
+import eslintPluginPlaywright from "eslint-plugin-playwright";
 import globals from "globals";
 
 export default defineConfig([
@@ -13,6 +14,7 @@ export default defineConfig([
     files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     languageOptions: {
       globals: {
+        ...globals.browser,
         ...globals.node,
       },
     },
@@ -40,7 +42,17 @@ export default defineConfig([
       "@typescript-eslint/no-explicit-any": "off",
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11Y.flatConfigs.recommended.rules,
       "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    plugins: {
+      playwright: eslintPluginPlaywright,
+    },
+    rules: {
+      ...eslintPluginPlaywright.configs.recommended.rules,
     },
   },
 ]);
