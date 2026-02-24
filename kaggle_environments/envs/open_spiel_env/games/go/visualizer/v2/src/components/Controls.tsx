9344f6c43da7acabff365d73be82da071f924861
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-02-24 18:32:44 +0000

    log player and move

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Controls.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Controls.tsx
@@ -22,9 +22,14 @@ export default function Controls() {
       // Check against game state captured by OpenSpiel. Matches.
       // console.log(options.replay.steps.at(options.step)?.boardState.board);
 
+      // Log the llm thoughts and player move. Note the agent names aren't
+      // returned in the same way as for chess, and they don't quite seem
+      // real in the replay data either.
       const step = options.replay.steps.at(options.step);
       const player = step!.players.find((player) => player.isTurn);
+      const [color, move] = step!.boardState.previous_move_a1!.split(' ');
       console.log(player?.thoughts);
+      console.log(`${player?.name} (${color}): ${move}`);
 
       setState(go);
     };
