0a569258b1e36ec57ccccc4cd1d2277f85626966
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-02-25 13:56:29 +0000

    feat: initial version (#724)

---
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/StyledGoboard.tsx
@@ -0,0 +1,54 @@
+import { useRef, useEffect } from 'react';
+import { createRenderer } from 'jgoboard';
+import useGoStore from '../stores/useGoStore';
+
+export default function StyledGoboard() {
+  const elem = useRef(null);
+  const go = useGoStore((state) => state.go);
+
+  useEffect(() => {
+    const options = {
+      board: go.board,
+      theme: {
+        margin: {
+          color: 'transparent',
+          normal: 0,
+          clipped: 0,
+        },
+        boardShadow: {
+          color: 'transparent',
+        },
+        padding: {
+          normal: 20,
+          clipped: 10,
+        },
+        grid: {
+          color: 'transparent',
+          x: 50,
+          y: 50,
+        },
+        coordinates: {
+          top: true,
+          right: true,
+          bottom: true,
+          left: true,
+          color: 'transparent',
+          font: 'normal 12px sans-serif',
+        },
+        textures: {
+          black: 'images/black.png',
+          white: 'images/white.png',
+          shadow: 'images/shadow.png',
+          board: 'images/board.jpg',
+        },
+      },
+      interactions: { enabled: false },
+    };
+
+    const renderer = createRenderer(elem.current, options);
+
+    return () => renderer.destroy();
+  }, [go]);
+
+  return <div ref={elem}></div>;
+}
