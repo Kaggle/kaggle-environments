5eefa28d14b1189b68d39d5a3fb94b8d9ad6cbf4
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-02-26 10:49:44 +0000

    react game renderer

---
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/components/GameRenderer.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { Chess } from 'chess.js';
+import { ChessPlayer, ChessStep, GameRendererProps } from '@kaggle-environments/core';
+import StyledBoard from '../components/StyledBoard';
+import Legend from './Legend';
+import useGoStore from '../stores/useChessStore';
+
+export default function GameRenderer(options: GameRendererProps<ChessStep[]>) {
+  const setState = useGoStore((state) => state.setState);
+
+  useEffect(() => {
+    const step = options.replay.steps.at(options.step);
+    const player = step!.players.find((player: ChessPlayer) => player.isTurn);
+
+    if (player) {
+      const history = options.replay.info!.stateHistory;
+      const fen = history.at(options.step);
+      const chess = new Chess(fen);
+      const move = chess.move(player.actionDisplayText!);
+
+      step!.players.map((p) => {
+        const opposite = { w: 'b', b: 'w' };
+        const color = p.isTurn ? move.color : opposite[move.color];
+        chess.setHeader(color, p.name);
+      });
+
+      console.log(player.thoughts);
+      console.log(`${player.name} (${move.color}): ${move.piece} ${move.from} -> ${move.to}`);
+
+      setState(chess);
+    }
+  }, [setState, options]);
+
+  return (
+    <div id="renderer">
+      <StyledBoard />
+      <Legend />
+    </div>
+  );
+}
