85f8bf1e169fb1fe1cc344361ff985535de2c6b8
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-03 18:28:59 +0000

    compact row and col lookup

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/GameRenderer.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/GameRenderer.tsx
@@ -20,28 +20,11 @@ export default function GameRenderer(options: GameRendererProps<GoStep[]>) {
       if (move === 'PASS') {
         game.pass();
       } else {
-        const index: { [key: string]: number } = {
-          'a': 0,
-          'b': 1,
-          'c': 2,
-          'd': 3,
-          'e': 4,
-          'f': 5,
-          'g': 6,
-          'h': 7,
-          'j': 8,
-          '1': 8,
-          '2': 7,
-          '3': 6,
-          '4': 5,
-          '5': 4,
-          '6': 3,
-          '7': 2,
-          '8': 1,
-          '9': 0,
-        };
-        const x = index[move.charAt(0)];
-        const y = index[move.charAt(1)];
+        type index = { [key: string]: number };
+        const rows: index = { '1': 8, '2': 7, '3': 6, '4': 5, '5': 4, '6': 3, '7': 2, '8': 1, '9': 0 };
+        const cols: index = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'j': 8 };
+        const y = rows[move.charAt(1)];
+        const x = cols[move.charAt(0)];
         game.playAt(y, x);
       }
     }
