2a3b46caf6cca1b59ec66ba81a6b509b0d83e963
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-04 10:21:27 +0000

    try useeffect, does stop double rendering

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/StyledBoard.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/StyledBoard.tsx
@@ -1,41 +1,53 @@
+import { useEffect } from 'react';
 import useGameStore from '../stores/useGameStore';
 import { GoBoard } from '../components/go-board';
+import { useState } from 'react';
 
 type CellValue = '.' | 'B' | 'W';
 
 export default function StyledBoard() {
   const game = useGameStore((state) => state.game);
-  const state = game.currentState();
-  const size = game.boardSize;
-  const step = state.moveNumber - 1;
 
-  const grid: CellValue[][] = [];
-  for (let y = 0; y < size; y++) {
-    const line: CellValue[] = [];
-    for (let x = 0; x < size; x++) {
-      const value = state.intersectionAt(y, x).value!;
+  const [size, setSize] = useState(0);
+  const [step, setStep] = useState(0);
+  const [grid, setGrid] = useState<CellValue[][]>([]);
+  const [played, setPlayed] = useState<{row: number, col: number} | null>(null);
+  const [captures, setCaptures] = useState({black: 0, white: 0});
+
+  useEffect(() => {
+    const state = game.currentState();
+    const size = game.boardSize;
+    const step = state.moveNumber;
+
+    const grid: CellValue[][] = Array.from({ length: size }, () => new Array(size));
+    state.intersections.forEach((item) => {
       const cells: { [key: string]: CellValue } = { 'black': 'B', 'white': 'W', 'empty': '.' };
-      line.push(cells[value]);
+      grid[item.y][item.x] = cells[item.value!];
+    });
+
+    let played = null;
+    if (state.playedPoint) {
+      played = {
+        row: state.playedPoint.y,
+        col: state.playedPoint.x,
+      };
     }
-    grid.push(line);
-  }
 
-  let lastPlayed = null;
-  if (state.playedPoint) {
-    lastPlayed = {
-      row: state.playedPoint.y,
-      col: state.playedPoint.x,
+    const captures = {
+      black: state.blackStonesCaptured,
+      white: state.whiteStonesCaptured,
     };
-  }
 
-  const captures = {
-    black: state.blackStonesCaptured,
-    white: state.whiteStonesCaptured,
-  };
-
+    setSize(size);
+    setStep(step);
+    setGrid(grid);
+    setPlayed(played);
+    setCaptures(captures);
+  }, [game, setSize, setStep, setGrid, setPlayed, setCaptures]);
+  
   return (
     <div id="board">
-      <GoBoard boardSize={game.boardSize} grid={grid} step={step} lastPlayed={lastPlayed} captures={captures} />
+      <GoBoard boardSize={size} grid={grid} step={step} lastPlayed={played} captures={captures} />
     </div>
   );
 }
