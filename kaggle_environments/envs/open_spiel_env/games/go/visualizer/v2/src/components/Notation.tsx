ecd0f4361ed6f45fdd229a4e21662fe3e20859c7
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-17 13:36:15 +0000

    feat: notation text update (#779)

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Notation.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Notation.tsx
@@ -18,73 +18,67 @@ export default memo(function Notation() {
       term: 'rethink',
       priority: 1,
       title: '',
-      text: `${agent} rethinks their decision`,
-    },
-    {
-      term: 'komi',
-      priority: 2,
-      title: `${agent} mentions Komi`,
-      text: `${komi} bonus points for going second`,
+      text: `${agent} rethinks their decision.`,
     },
     {
       term: 'monkey jump',
       priority: 1,
-      title: `${agent} mentions Monkey Jump`,
-      text: `Reducing opposition territory at the boards edge`,
+      title: `${agent} mentions Monkey Jump:`,
+      text: `Reducing opposition territory at the boards edge.`,
     },
     {
       term: 'ladder',
       priority: 1,
-      title: `${agent} mentions Ladder`,
-      text: `Capturing stones in a zigzag pattern`,
+      title: `${agent} mentions Ladder:`,
+      text: `Capturing stones in a zigzag pattern.`,
     },
     {
       term: "tiger's mouth",
       priority: 1,
-      title: `${agent} mentions Tiger's Mouth`,
-      text: `A three-stone shape that creates a "trap"`,
+      title: `${agent} mentions Tiger's Mouth:`,
+      text: `A three-stone shape that creates a "trap".`,
     },
     {
       term: "horse's head",
       priority: 1,
-      title: `${agent} mentions Horse's Head`,
-      text: `An L shaped flexible attacking position`,
+      title: `${agent} mentions Horse's Head:`,
+      text: `An L shaped flexible attacking position.`,
     },
     {
       term: 'wedge',
       priority: 1,
-      title: `${agent} mentions Wedge`,
-      text: `Playing between two opponent stones`,
+      title: `${agent} mentions Wedge:`,
+      text: `Playing between two opponent stones.`,
     },
     {
       term: "crane's nest",
       priority: 1,
-      title: `${agent} mentions Crane's Nest`,
-      text: `A group trap that resembles a "nest"`,
+      title: `${agent} mentions Crane's Nest:`,
+      text: `A group trap that resembles a "nest".`,
     },
     {
       term: 'three crows',
       priority: 1,
-      title: `${agent} mentions Three Crow's`,
-      text: `Guarding a corner with three stones`,
+      title: `${agent} mentions Three Crow's:`,
+      text: `Guarding a corner with three stones.`,
     },
     {
       term: 'false eye',
       priority: 1,
-      title: `${agent} mentions False Eye`,
-      text: `A space that looks safe, but isn't`,
+      title: `${agent} mentions False Eye:`,
+      text: `A space that looks safe, but isn't.`,
     },
     {
       term: 'clamp',
       priority: 1,
-      title: `${agent} mentions Clamp`,
-      text: `Playing both sides of an opponent's stone`,
+      title: `${agent} mentions Clamp:`,
+      text: `Playing both sides of an opponent's stone.`,
     },
     {
       term: 'iron pillar',
-      title: `${agent} mentions Iron Pillar`,
+      title: `${agent} mentions Iron Pillar:`,
       priority: 1,
-      text: `A defensive, vertical two-stone tower`,
+      text: `A defensive, vertical two-stone tower.`,
     },
   ];
   const matches = searches.filter((search) => player.thoughts?.toLowerCase().includes(search.term));
@@ -95,15 +89,31 @@ export default memo(function Notation() {
   if (data.duration_success_only_secs > 60 * duration) {
     matches.push({
       term: 'duration',
-      priority: 1,
+      priority: 2,
       title: '',
-      text: `${agent} took over ${duration} minutes to decide`,
+      text: `${agent} thought for over ${duration} minutes.`,
+    });
+  }
+  if (game.moveNumber() === 1) {
+    matches.push({
+      term: 'goes first',
+      priority: 0,
+      title: `${agent} goes first:`,
+      text: `Unlike Chess, black plays first.`,
+    });
+  }
+  if (game.moveNumber() === 2) {
+    matches.push({
+      term: 'komi',
+      priority: 0,
+      title: `${agent} gets Komi:`,
+      text: `A ${komi} point bonus for playing second.`,
     });
   }
 
   if (matches.length === 0) return null;
 
-  const notation = matches.toSorted((a, b) => b.priority - a.priority)[0];
+  const notation = matches.toSorted((a, b) => a.priority - b.priority)[0];
 
   return (
     <div className={styles.notation}>
