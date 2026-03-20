9febadca226968987ce1300c60ea7e619e7b3e44
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-20 10:54:23 +0000

    feat: hacky proof-of-concept that works

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { createReplayVisualizer, ReplayAdapter, defaultGetStepRenderTime } from '@kaggle-environments/core';
 import { goTransformer } from './transformers/goTransformer';
+import { GoStep } from './transformers/goReplayTypes';
 import GameRenderer from './components/GameRenderer';
 import useGameStore from './stores/useGameStore';
 import './App.css';
@@ -22,6 +23,38 @@ export default function App() {
         console.log("getStepRenderTime", step.step);
         const time = defaultGetStepRenderTime(step, replayMode, speedModifier);
         const game = useGameStore.getState().game;
+
+        const [, move] = (step as GoStep).boardState.previous_move_a1!.split(' ');
+        if (move === 'PASS') {
+          game.pass();
+        } else {
+          type index = { [key: string]: number };
+          const cols: index = {
+            'a': 0,
+            'b': 1,
+            'c': 2,
+            'd': 3,
+            'e': 4,
+            'f': 5,
+            'g': 6,
+            'h': 7,
+            'j': 8,
+            'k': 9,
+            'l': 10,
+            'm': 11,
+            'n': 12,
+            'o': 13,
+            'p': 14,
+            'q': 15,
+            'r': 16,
+            's': 17,
+            't': 18,
+          };
+          const y = game.boardSize - parseInt(move.slice(1));
+          const x = cols[move.charAt(0)];
+          game.playAt(y, x);
+        }
+
         const state = game.currentState();
 
         const isPass = state.pass;
