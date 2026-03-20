a7504381c264612cb74b630a5647b0f9778ef3ba
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-20 14:39:48 +0000

    chore: move tranformer and getStepRenderTime definition out of replay adapter config object

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
@@ -1,11 +1,8 @@
 import { useCallback } from 'react';
-import { createReplayVisualizer, ReplayAdapter, defaultGetStepRenderTime } from '@kaggle-environments/core';
-import { goTransformer } from './transformers/goTransformer';
-import { GoStep } from './transformers/goReplayTypes';
+import { createReplayVisualizer, ReplayAdapter } from '@kaggle-environments/core';
+import { transformer } from './transformers/transformer';
+import { getStepRenderTime } from './utils/getStepRenderTime';
 import GameRenderer from './components/GameRenderer';
-import useGameStore from './stores/useGameStore';
-import usePreferences from './stores/usePreferences';
-import { detectHeroType } from './utils/heroTypes';
 import './App.css';
 
 export default function App() {
@@ -16,57 +13,8 @@ export default function App() {
       gameName,
       GameRenderer,
       ui,
-      transformer: (replay) => ({
-        ...replay,
-        steps: goTransformer(replay),
-        isTransformed: true,
-      }),
-      getStepRenderTime: (step, replayMode, speedModifier) => {
-        const time = defaultGetStepRenderTime(step, replayMode, speedModifier);
-        const showHeroAnimations = usePreferences.getState().showHeroAnimations;
-        const reducedMotion = usePreferences.getState().reducedMotion;
-
-        if (reducedMotion || !showHeroAnimations) return time;
-
-        const game = useGameStore.getState().game;
-        // Temporary hack: The step render time is calculated before the step
-        // is rendered, so make the move for the most recent step before
-        // working out if it's playback duration needs adjusting.
-        const [, move] = (step as GoStep).boardState.previous_move_a1!.split(' ');
-        if (move === 'PASS') {
-          game.pass();
-        } else {
-          type index = { [key: string]: number };
-          const cols: index = {
-            'a': 0,
-            'b': 1,
-            'c': 2,
-            'd': 3,
-            'e': 4,
-            'f': 5,
-            'g': 6,
-            'h': 7,
-            'j': 8,
-            'k': 9,
-            'l': 10,
-            'm': 11,
-            'n': 12,
-            'o': 13,
-            'p': 14,
-            'q': 15,
-            'r': 16,
-            's': 17,
-            't': 18,
-          };
-          const y = game.boardSize - parseInt(move.slice(1));
-          const x = cols[move.charAt(0)];
-          game.playAt(y, x);
-        }
-
-        if (detectHeroType(game) !== null) return 1000 * 5;
-
-        return time;
-      },
+      transformer,
+      getStepRenderTime,
     });
     createReplayVisualizer(element, adapter);
   }, []);
