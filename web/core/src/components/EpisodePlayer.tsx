a7f6164501d68a3cf3f442bc73d151f2bd99a766
Domino Weir <dominoweir@gmail.com>
2026-03-23 14:20:37 -0400

    Exclude focused buttons from keyboard shortcuts (#821)
    See chat for discussion of how this impacts accessibility quality for
    controlling Go settings

--- web/core/src/components/EpisodePlayer.tsx
+++ web/core/src/components/EpisodePlayer.tsx
@@ -238,7 +238,7 @@ export function EpisodePlayer<TSteps extends BaseGameStep[] = BaseGameStep[]>({
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       const target = event.target as HTMLElement;
-      if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA') {
+      if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.tagName === 'BUTTON') {
         return;
       }
 
