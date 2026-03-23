e6327281e6528db9905f4c584595a3adb603841f
Pim de Wit <pim@stinkstudios.com>
2026-03-23 21:24:39 +0000

    fix(go): simplify ribbon, fix z-order rendering on ios (#827)
    Fixes this bug on iOS (due to mixing grid + css filters):
    
    
    
    https://github.com/user-attachments/assets/01ca547a-b39f-44e3-a418-8cbdb087d942
    
    ---------
    
    Co-authored-by: Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Ribbon.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Ribbon.tsx
@@ -5,41 +5,26 @@ interface Props {
   children: ReactNode;
 }
 
-export function Ribbon({ children }: Props) {
+export function FlagEnd() {
   return (
-    <div className={styles.banner}>
-      <RibbonGraphic />
-      <div className={styles.text}>{children}</div>
-    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 55 116" width="55" height="114">
+      <path
+        fill="#fff"
+        stroke="currentColor"
+        strokeWidth="2"
+        vectorEffect="non-scaling-stroke"
+        d="M52 113H3q-2-1-2-3l28-51v-4L1 4q0-2 2-3h49l2 2v108z"
+      />
+    </svg>
   );
 }
 
-function RibbonGraphic() {
+export function Ribbon({ children }: Props) {
   return (
-    <div className={styles.ribbon}>
-      <div className={styles.start}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 55 114">
-          <path
-            fill="#fff"
-            stroke="currentColor"
-            strokeWidth="2"
-            vectorEffect="non-scaling-stroke"
-            d="M52 113H3q-2-1-2-3l28-51v-4L1 4q0-2 2-3h49l2 2v108z"
-          />
-        </svg>
-      </div>
-      <div className={styles.middle} />
-      <div className={styles.end}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 55 114">
-          <path
-            fill="#fff"
-            stroke="currentColor"
-            strokeWidth="2"
-            vectorEffect="non-scaling-stroke"
-            d="M3 1h49l2 3-28 51v4l28 51-2 3H3l-2-2V3z"
-          />
-        </svg>
-      </div>
+    <div className={`grid-pile ${styles.ribbon}`}>
+      <FlagEnd />
+      <FlagEnd />
+      <div className={styles.text}>{children}</div>
     </div>
   );
 }
