c6bd4c0b7afd580af60b58965c901b0c3bab1cad
Pim de Wit <pim@stinkstudios.com>
2026-03-24 10:44:09 +0000

    fix(go): add versioned svg sprites, prevent vite's asset inlining

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/vite.config.ts
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/vite.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig, mergeConfig } from 'vite';
+import { mergeConfig, type UserConfig } from 'vite';
 import baseConfig from '../../../../../../../web/vite.config.base';
 
-export default mergeConfig(baseConfig, defineConfig({}));
+export default mergeConfig(baseConfig as UserConfig, {
+  build: {
+    // Prevent Vite from transforming files to base64. We deliberately want
+    // external assets for caching efficiency.
+    assetsInlineLimit: 0,
+  },
+});
