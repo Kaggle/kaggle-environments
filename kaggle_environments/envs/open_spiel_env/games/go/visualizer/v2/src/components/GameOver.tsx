ad1db40804d0916e387ed7f1fda0fb63b6601b99
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-26 11:29:35 +0000

    fix: game over total moves (#852)

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/GameOver.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/GameOver.tsx
@@ -76,7 +76,7 @@ export default function GameOver() {
     }
   }
 
-  const totalMoves = options.replay.steps.length;
+  const totalMoves = game.moveNumber();
   const allDurations = [...durations.black, ...durations.white];
   const gameDuration = allDurations.reduce((a, b) => a + b, 0);
 
