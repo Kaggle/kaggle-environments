f53d917aefc3c68916ac314ab13adb1377d80c81
Pim de Wit <pim@stinkstudios.com>
2026-03-26 11:20:10 +0000

    feat(go): add mute button (#853)
    <img width="137" height="230" alt="Screenshot 2026-03-26 at 11 04 35"
    src="https://github.com/user-attachments/assets/43d5e5b1-6d00-4243-9930-dd9ca1a1d38d"
    />

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/BoardControls.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/BoardControls.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from 'react';
 import arrowPath from '../assets/arrow.webp';
+import svgSymbolPath from '../assets/icons.svg?url';
+import usePreferences from '../stores/usePreferences.ts';
+import useGameStore from '../stores/useGameStore';
+import popoverStyles from './WithPopover.module.css';
 import { WithPopover } from './WithPopover.tsx';
 import { FeatureToggles } from './FeatureToggles.tsx';
-import useGameStore from '../stores/useGameStore';
 import styles from './BoardControls.module.css';
 
 export default function BoardControls() {
   const game = useGameStore((state) => state.game);
+  const { toggle, soundEnabled } = usePreferences();
+
+  const iconPath = useMemo(() => {
+    const soundIcon = soundEnabled ? 'sound-on' : 'sound-off';
+    return `${svgSymbolPath}#${soundIcon}`;
+  }, [soundEnabled]);
 
   // React 18 doesn't support the `inert` HTML attribute as a prop, so we
   // set it imperatively via a ref callback. This can be replaced with a
@@ -24,6 +34,25 @@ export default function BoardControls() {
           placing black and white stones. This game is based on Tromp-Taylor rules.
         </p>
       </WithPopover>
+      <label className={`${popoverStyles.trigger} ${styles.soundToggle}`}>
+        <input
+          type="checkbox"
+          checked={soundEnabled}
+          onChange={() => toggle('soundEnabled')}
+          className="visually-hidden"
+        />
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <use xlinkHref={iconPath} />
+        </svg>
+        <span className="visually-hidden">Sound</span>
+      </label>
       <WithPopover id="settings" icon="settings" label="Settings">
         <FeatureToggles />
       </WithPopover>
