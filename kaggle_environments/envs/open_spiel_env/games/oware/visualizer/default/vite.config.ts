a838e9a00a88a4b2cc50e59579fb4b390d4480d9
Domino Weir <dominoweir@google.com>
2026-03-27 18:15:04 +0000

    Remove game examples, save for later

--- kaggle_environments/envs/open_spiel_env/games/oware/visualizer/default/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, mergeConfig } from 'vite';
-import baseConfig from '../../../../../../../web/vite.config.base';
-
-export default mergeConfig(
-  baseConfig,
-  defineConfig({
-    publicDir: 'replays',
-  })
-);
