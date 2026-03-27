d50c39f838345eafd57ffaaf114fe1f6882210c5
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-03-27 10:20:10 +0000

    feat: game over log message

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/utils/getStepLabel.ts
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/utils/getStepLabel.ts
@@ -1,20 +1,29 @@
 import { BaseGameStep } from '@kaggle-environments/core';
+import { GoStep } from '../transformers/goReplayTypes';
 
 export function getStepLabel(step: BaseGameStep) {
   const player = step.players.find((p) => p.isTurn);
 
-  if (player === undefined && step.step === 0) {
-    const black = step.players.at(0)?.name ?? 'Black';
-    const white = step.players.at(1)?.name ?? 'White';
-    return `${black} vs. ${white}`;
+  if (player) {
+    const color = player.id ? 'white' : 'black';
+    const move = player.actionDisplayText?.toUpperCase() ?? '';
+    if (move === 'PASS') return `Played ${move}`;
+    return `Played the ${color} stone on ${move}`;
   }
 
-  if (player === undefined) return '';
+  const blackName = step.players.at(0)?.name ?? 'Black';
+  const whiteName = step.players.at(1)?.name ?? 'White';
 
-  const color = player?.id ? 'white' : 'black';
-  const move = player?.actionDisplayText?.toUpperCase() ?? '';
+  // Game Start
+  if (step.step === 0) {
+    return `${blackName} vs. ${whiteName}`;
+  }
 
-  if (move === 'PASS') return `Played ${move}`;
+  // Game Over
+  const winner = (step as GoStep).winner;
+  if (winner) {
+    return `Winner is ${winner === 'black' ? blackName : whiteName}`;
+  }
 
-  return `Played the ${color} stone on ${move}`;
+  return '';
 }
