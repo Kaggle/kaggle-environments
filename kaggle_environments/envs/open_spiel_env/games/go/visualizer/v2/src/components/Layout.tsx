4b237089adb1d6915b1c47145a8bd1dfa3d41b5a
Pim de Wit <pim@stinkstudios.com>
2026-03-30 14:31:07 +0100

    feat(go): preload assets (#888)

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Layout.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import HiddenHeader from './HiddenHeader';
 import BoardControls from './BoardControls';
 import GameBoard from './GameBoard';
@@ -8,11 +8,19 @@ import VersusBanner from './VersusBanner';
 import GameOver from './GameOver';
 import HeroAnimation from './HeroAnimation';
 import SoundEffects from './SoundEffects';
+import usePreloader from '../stores/usePreloader';
+import { assetsReady } from '../utils/preloadAssets';
 import styles from './Layout.module.css';
 
 export default memo(function Layout() {
+  const loaded = usePreloader((s) => s.pixiReady && s.assetsReady);
+
+  useEffect(() => {
+    assetsReady.then(() => usePreloader.getState().setAssetsReady());
+  }, []);
+
   return (
-    <main id="go-playable-area" className={styles.playableArea}>
+    <main id="go-playable-area" className={styles.playableArea} data-loaded={loaded || undefined}>
       <HiddenHeader />
       <div className={styles.board}>
         <BoardControls />
