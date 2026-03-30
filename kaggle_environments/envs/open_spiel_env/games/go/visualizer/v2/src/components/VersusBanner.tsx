5bbac2e1e94d6e20e2e76df1a2b3d842b6cd093b
Pim de Wit <pim@stinkstudios.com>
2026-03-30 14:31:07 +0100

    feat(go): preload assets (#888)

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/VersusBanner.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/components/VersusBanner.tsx
@@ -1,17 +1,19 @@
 import { AnimatePresence, motion } from 'motion/react';
 import { Ribbon } from './Ribbon';
 import useGameStore from '../stores/useGameStore';
+import usePreloader from '../stores/usePreloader';
 import { useTransition } from '../hooks/useReducedMotion';
 import styles from './VersusBanner.module.css';
 
 export default function VersusBanner() {
   const game = useGameStore((state) => state.game);
+  const loaded = usePreloader((s) => s.pixiReady && s.assetsReady);
   const enterTransition = useTransition({ duration: 1.3, ease: [0.22, 1.15, 0.36, 1] });
   const exitTransition = useTransition({ duration: 0.3 });
 
   return (
     <AnimatePresence>
-      {game.gameStart && (
+      {loaded && game.gameStart && (
         <motion.div
           className={styles.versusBanner}
           aria-hidden="true"
