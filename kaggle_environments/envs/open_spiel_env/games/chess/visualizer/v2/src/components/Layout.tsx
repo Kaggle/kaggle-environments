43dfca36378ee920e8858c79cc32717efe6ca8e1
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-04-08 18:15:45 +0100

    chore: remove dead components

--- kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/components/Layout.tsx
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/components/Layout.tsx
@@ -1,18 +1,10 @@
 import { memo } from 'react';
-import Meter from './Meter';
 import GameBoard from './GameBoard';
-import Legend from './Legend';
-import Openings from './Openings';
-import GameOver from './GameOver';
 
 export default memo(function Layout() {
   return (
     <div id="renderer">
-      <Meter />
       <GameBoard />
-      <Legend />
-      <Openings />
-      <GameOver />
     </div>
   );
 });
