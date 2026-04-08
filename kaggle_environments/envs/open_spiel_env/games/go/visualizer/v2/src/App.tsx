1a0281f5b32f8fd4e670a7cd4fd966d795b72c3d
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-04-08 11:15:38 +0100

    feat: consistent game name across core and visualizer specific analytics

--- kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
+++ kaggle_environments/envs/open_spiel_env/games/go/visualizer/v2/src/App.tsx
@@ -3,12 +3,13 @@ import { createReplayVisualizer, ReplayAdapter } from '@kaggle-environments/core
 import { transformer } from './transformers/transformer';
 import { getStepRenderTime } from './utils/getStepRenderTime';
 import { getStepLabel } from './utils/getStepLabel';
+import { GAME_NAME } from './utils/analytics';
 import GameRenderer from './components/GameRenderer';
 import './App.css';
 
 export default function App() {
   const init = useCallback((element: HTMLDivElement) => {
-    const gameName = 'open_spiel_go';
+    const gameName = GAME_NAME;
     const ui = 'side-panel';
     const adapter = new ReplayAdapter({
       gameName,
