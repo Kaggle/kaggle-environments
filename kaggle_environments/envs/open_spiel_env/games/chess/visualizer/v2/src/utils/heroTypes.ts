ac4b05c83c529bf547631bbed9bcc8947a332452
Jim Hunt <257586852+jim-at-stink@users.noreply.github.com>
2026-04-09 15:09:55 +0100

    feat: add hero types (#935)

--- kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/utils/heroTypes.ts
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/utils/heroTypes.ts
@@ -9,6 +9,23 @@ export enum HeroTypes {
 }
 
 export function detectHeroType(game: Chess): HeroTypes | null {
-  if (game.isCheckmate()) return HeroTypes.CHECKMATE;
+  const move = game.history({ verbose: true }).at(-1);
+  const pieceCount = game
+    .board()
+    .flat()
+    .filter((p) => p !== null).length;
+
+  const isCheckmate = game.isCheckmate();
+  const isQueenLoss = move?.captured === 'q';
+  const isPromotion = move?.isPromotion();
+  const isCastling = move?.isKingsideCastle() || move?.isQueensideCastle();
+  const isFirstCapture = move?.isCapture() && pieceCount === 31;
+
+  if (isCheckmate) return HeroTypes.CHECKMATE;
+  if (isQueenLoss) return HeroTypes.QUEEN_LOSS;
+  if (isPromotion) return HeroTypes.PROMOTION;
+  if (isCastling) return HeroTypes.CASTLING;
+  if (isFirstCapture) return HeroTypes.FIRST_CAPTURE;
+
   return null;
 }
