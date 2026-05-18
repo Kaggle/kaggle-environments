93170b2b8948981873c8364ef4c26c9040c97507
Brandon Vu <51305295+brandonvu12@users.noreply.github.com>
2026-05-18 13:01:20 -0400

    Feat:Update Reward system for memory games (#968)
    Update the reward system to count the team with the most wins in an
    episode as the winner rather than the team that won the last game of the
    episode.
    
    Also update the UI to handle the memory games or single game episodes.

--- kaggle_environments/envs/word_association/scripts/generate_replay.py
+++ kaggle_environments/envs/word_association/scripts/generate_replay.py
@@ -2,7 +2,7 @@ import json
 import os
 from kaggle_environments import make
 
-env = make("word_association", debug=True)
+env = make("word_association", configuration={"games_per_episode": 1}, debug=True)
 env.run(["random", "random", "random", "random"])
 
 replay_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "visualizer", "default", "replays")
